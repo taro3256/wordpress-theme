3166ad6325bf67ed3e6e129f7149101f71fd1889
amaisuna <taro30143014@gmail.com>
2019-08-21 07:31:23 +0900

    tmplate

--- functions.php
+++ functions.php
@@ -1,10 +1,28 @@
 <?php
-//テーマのセットアップ
-// titleタグをhead内に生成する
-add_theme_support('title-tag');
-// HTML5でマークアップさせる
-add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
-// Feedのリンクを自動で生成する
-add_theme_support('automatic-feed-links');
-//アイキャッチ画像を使用する設定
-add_theme_support('post-thumbnails');
+add_theme_support( 'title-tag' );
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+add_theme_support( 'automatic-feed-links' );
+add_theme_support( 'post-thumbnails' );
+
+//カスタムメニュー
+register_nav_menu( 'header-nav',  ' ヘッダーナビゲーション ' );
+register_nav_menu( 'footer-nav',  ' フッターナビゲーション ' );
+
+//メニューボタンjs呼び出し
+function navbutton_scripts(){
+  wp_enqueue_script( 'navbutton_script', get_template_directory_uri() .'/js/navbutton.js', array('jquery') );
+}
+add_action( 'wp_enqueue_scripts' , 'navbutton_scripts' );
+
+//サイドバーにウィジェット追加
+function widgetarea_init() {
+register_sidebar(array(
+    'name'=>'サイドバー',
+    'id' => 'side-widget',
+    'before_widget'=>'<div id="%1$s" class="%2$s sidebar-wrapper">',
+    'after_widget'=>'</div>',
+    'before_title' => '<h4 class="sidebar-title">',
+    'after_title' => '</h4>'
+    ));
+}
+add_action( 'widgets_init', 'widgetarea_init' );
--- home.php
+++ home.php
@@ -2,15 +2,6 @@
 <div class="container">
   <div class="contents">
 
-    <?php if(is_category() || is_tag()): ?>
-      <h1><?php single_cat_title() ?>の記事一覧</h1>
-    <?php elseif(is_year()): ?>
-      <h1><?php the_time("Y年") ?>の記事一覧</h1>
-    <?php elseif(is_month()): ?>
-      <h1><?php the_time("Y年m月") ?>の記事一覧</h1>
-    <?php endif; ?>
-
-
     <?php if(have_posts()): while(have_posts()): the_post(); ?>
     <article <?php post_class( 'kiji-list' ); ?>>
       <a href="<?php the_permalink(); ?>">
